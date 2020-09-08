83a80021666ceda260f6a88b16842f4df20b1a88
B-Aniruddha <70235936+B-Aniruddha@users.noreply.github.com>
2020-09-08 19:51:29 -0400

    Fixed failed test for creation of item

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -3,7 +3,7 @@ const { Store, Item } = require("../src/gilded_rose");
 describe("gilded rose", function () {
     describe("item creation test", function () {
         it("should be default", function () {
-            const item = new Item("foo", 0, 0);
+            const item = new Item("default", 0, 0);
             expect(item.name).toBe("default");
         });
     });
