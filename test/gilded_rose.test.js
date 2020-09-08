f843b9742c1703351700139cfce62b0e4b90a376
B-Aniruddha <70235936+B-Aniruddha@users.noreply.github.com>
2020-09-08 19:48:19 -0400

    First test on creation of an item through Item class

---
+++ test/gilded_rose.test.js
@@ -0,0 +1,10 @@
+const { Store, Item } = require("../src/gilded_rose");
+
+describe("gilded rose", function () {
+    describe("item creation test", function () {
+        it("should be default", function () {
+            const item = new Item("foo", 0, 0);
+            expect(item.name).toBe("default");
+        });
+    });
+});
