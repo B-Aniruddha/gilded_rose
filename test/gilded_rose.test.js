c7bfea0e6864b4fdae27d362d6f527d596af72f9
B-Aniruddha <70235936+B-Aniruddha@users.noreply.github.com>
2020-09-08 20:54:06 -0400

    Fixed 3rd test for Aged Brie item

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -69,7 +69,7 @@ describe("gilded rose", function () {
             const gildedRose = new Store([new Item('Aged Brie', -1, 50)]);
             const items = gildedRose.updateQuality();
 
-            expect(items[0].quality).toBe(51);
+            expect(items[0].quality).toBe(50);
         });
 
     });
