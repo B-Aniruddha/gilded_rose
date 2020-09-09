e528c62fe1df0c87a1bb721e15dd202597ca690d
B-Aniruddha <70235936+B-Aniruddha@users.noreply.github.com>
2020-09-08 22:29:08 -0400

    Fixed failed 6th test for TAFKAL80ETC concert item

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -151,7 +151,7 @@ describe("gilded rose", function () {
             const gildedRose = new Store([new Item('Backstage passes to a TAFKAL80ETC concert', 4, 48)]);
             const items = gildedRose.updateQuality();
 
-            expect(items[0].quality).not.toBe(50);
+            expect(items[0].quality).not.toBe(51);
         });
     });
 });
