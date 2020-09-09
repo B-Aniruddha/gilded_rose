69020a92376f69874796850883ef37bbe96dd77f
B-Aniruddha <70235936+B-Aniruddha@users.noreply.github.com>
2020-09-08 22:11:53 -0400

    Added 2nd test for TAFKAL80ETC concert item

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -120,4 +120,10 @@ describe("gilded rose", function () {
         });
     });
 
+    it('should increase quality by 1 for any item when sellIn > 11 ', function () {
+        const gildedRose = new Store([new Item('Backstage passes to a TAFKAL80ETC concert', 12, 49)]);
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].quality).toBe(49);
+    });
 });
